d8bf5e9dd8a7321503732a521d10d8ec5228d6d3
AviTirto <avi.tirto@gmail.com>
2025-06-16 00:28:46 -0500

    Boilerplate actions for RoomHub

--- MovieMatch/Hubs/RoomHub.cs
+++ MovieMatch/Hubs/RoomHub.cs
@@ -9,5 +9,21 @@ namespace MovieMatch.Hubs
             await Groups.AddToGroupAsync(Context.ConnectionId, roomCode);
             await Clients.Group(roomCode).SendAsync("UserJoined", Context.ConnectionId);
         }
+
+        public async Task LeaveRoomGroup(string roomCode)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomCode);
+            await Clients.Group(roomCode).SendAsync("UserLeft", Context.ConnectionId);
+        }
+
+        public async Task StartGame(string roomCode)
+        {
+            await Clients.Group(roomCode).SendAsync("GameStarted");
+        }
+
+        public async Task Swipe(string roomCode, string movieId)
+        {
+            
+        }
     }
 }
